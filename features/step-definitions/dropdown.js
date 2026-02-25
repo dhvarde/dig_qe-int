2317f8a5db09717f91f56d497100f0f58307db99
HVarde <Hrishikesh.Varde@rockwellautomation.com>
2026-02-25 09:24:42 +0530

    Test_Project : Fix Index, Dropdown features
    - Remove redundant code in dropdown.js and imports
    - Update stepDefinitions as per feature
    - Update regEx in index.js to match appropriate html titles
    - Fix spell errors in feature

--- features/step-definitions/dropdown.js
+++ features/step-definitions/dropdown.js
@@ -1,14 +1,15 @@
-import { When, Then, world } from "@cucumber/cucumber";
+import { Given, When, Then } from "@cucumber/cucumber";
 import { expect } from "@wdio/globals";
 import dropdownPage from "../pageobjects/dropdown.page.js";
 
+Given('I am on the Dropdown page', async function() {
+  await dropdownPage.open();
+});
+
 When(/^I select "(.+)"$/, async function (option) {
-  world.hello = "hello";
-  console.log(world);
   await dropdownPage.select(option);
 });
 
 Then(/^The dropdown value should be "(.+)"$/, async function (option) {
-  console.log(world);
-  expect(await dropdownPage.selectedOptionText()).toBe(option);
+  await expect(await dropdownPage.selectedOptionText()).toBe(option);
 });
--- features/step-definitions/index.js
+++ features/step-definitions/index.js
@@ -2,10 +2,6 @@ import { Given, When, Then } from "@wdio/cucumber-framework";
 import Page from "../pageobjects/page.js";
 const index = new Page();
 
-Given(/^I am on the (.+) page$/, async (page) => {
-  await index.open(page);
-});
-
 Given("I am at the index page", async function () {
   await index.open();
 });
@@ -15,10 +11,9 @@ When(/^I click the (.+) link$/, async function (page) {
   await index.click(page);
 });
 
-Then("I should be driected to the selected page", async function () {
+Then("I should be directed to the selected page", async function () {
   const html = await $("*").getHTML();
-  console.log(html);
-  expect(html).toMatch(new RegExp(`/h3.+${this.page}.+h3/`, "gm"));
+  expect(html).toMatch(new RegExp(`\<h3\>${this.page}\<\/h3\>`, "gm"));
   // const header = await $("h3");
   // expect(header).toHaveTextContaining(this.page);
 });
